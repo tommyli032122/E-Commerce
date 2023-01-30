6bce0ba6507928ac4ce53f1c38e5f1ab741b570c
tommyli032122 <tommyli032122@gmail.com>
2023-01-30 14:48:24 +0800

    01/30update

--- E-Commerce_practice_project/main.js
+++ E-Commerce_practice_project/main.js
@@ -4,6 +4,7 @@ document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
     cart.classList.remove('active');
     user.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 let cart = document.querySelector('.cart');
@@ -11,6 +12,7 @@ document.querySelector('#cart-icon').onclick = () => {
     cart.classList.toggle('active');
     search.classList.remove('active');
     user.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 let user = document.querySelector('.user');
@@ -18,7 +20,24 @@ document.querySelector('#user-icon').onclick = () => {
     user.classList.toggle('active');
     search.classList.remove('active');
     cart.classList.remove('active');
+    navbar.classList.remove('active');
 }
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+    user.classList.remove('active');
+    search.classList.remove('active');
+    cart.classList.remove('active');
+}
+
+// window.onscroll = () => {
+//     search.classList.remove('active');
+//     cart.classList.remove('active');
+//     user.classList.remove('active');
+//     navbar.classList.remove('active');
+// }
+
 //search-box
 document.querySelector('#search-input').addEventListener('input', filterList);
 
